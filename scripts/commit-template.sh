3209afa5ad19a90d690bf122515fc3e64492e5a6
Abraham White <abelincoln.white@gmail.com>
2018-07-17 14:55:07 -0600

    chore(workflow): add commitlint and commit template
    Adds commitlint to the repository to validate commit messages. Also adds
    a script to add a default git commit template. This can usually be done
    with `git config commit.template <FILE>`, however, this would require
    all contributors to run the command to get this functionality. The
    script is run with the git hooks installed by husky, and so is set up
    when husky is installed (with npm install).

---
+++ scripts/commit-template.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+FILE="${1:-.commit-msg}"
+
+fullpath() {
+  if command -v realpath >/dev/null 2>&1; then
+    realpath "$@"
+  elif command -v python >/dev/null 2>&1; then
+    python -c "import os,sys; print os.path.realpath(sys.argv[1])" "$@"
+  else
+    echo >&2 "Cannot resolve $*; Aborting"
+    exit 1
+  fi
+}
+
+TEMPLATE="$(fullpath "$FILE")"
+
+if [ ! -f "$TEMPLATE" ]; then
+  echo >&2 "Could not find template file at $TEMPLATE"
+  exit 1
+fi
+
+shift $#
+set -- "$GIT_PARAMS"
+
+# only apply custom messages when run as a standalone `git commit`
+# ignore `--ammend` commits, merges, or commits with `-m`
+if [ $# -gt 1 ]; then
+  # GIT_PARAMS had more than one arg
+  exit 0
+fi
+
+# if no params are present then the npm script is being run
+# directly, and not via a hook
+if [ -z "$1" ]; then
+  cat "$TEMPLATE"
+  exit 0
+fi
+
+TARGET="$(fullpath "$1")"
+TEMP="$(mktemp commitXXX)"
+cat "$TEMPLATE" >> "$TEMP"
+cat "$TARGET" >> "$TEMP"
+mv "$TEMP" "$TARGET"
