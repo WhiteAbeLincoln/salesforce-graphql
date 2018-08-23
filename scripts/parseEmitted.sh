a6006eac3d40fa4f0336e201baa0496fab219312
Abraham White <abelincoln.white@gmail.com>
2018-08-23 12:32:43 -0600

    build(process): update postpack script
    Simplifies the build process so that emitted files are listed
    when copying from the build directory using cp -v, rather than
    for each part of the build process (tsc, etc)

--- scripts/parseEmitted.sh
+++ scripts/parseEmitted.sh
@@ -7,7 +7,14 @@ OUTFILE="${1:-/dev/stdout}"
 while read -r line; do
   case "$line" in
     TSFILE:*)
+      # tsc --listEmittedFiles output
       echo "$line" | cut -d' ' -f 2- - >> "$OUTFILE"
+      ;;
+    *' -> '*)
+      # cp verbose output
+      # cp -v may print relative paths. we need absolute from root for the postpack script to work
+      # using realpath means users must have gnu coreutils installed :(
+      echo "$line" | awk -F"' -> '" '{print substr($2, 1, length($2)-1)}' | xargs realpath >> "$OUTFILE"
       ;;
     *)
       echo "$line"
--- scripts/postpack.sh
+++ scripts/postpack.sh
@@ -2,9 +2,7 @@
 
 # Cleans up emitted build files and directories
 
-awk '{gsub("'"$PWD/build"'", "'"$PWD"'"); printf("%s%c", $0, 0)}' ./*.emit | xargs -0 rm
-
-# remove empty directories
-find . -type d -empty -not -path "*/node_modules/*" -not -path "*/.git/*" -not -path "src/*" -delete
+# if any paths are relative to the build directory, make them relative to root
+awk '{gsub("'"$PWD/build/"'", "'"$PWD/"'"); printf("%s%c", $0, 0)}' ./*.emit | xargs -0 rm -rf
 
 rm ./*.emit
